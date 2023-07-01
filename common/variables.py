6dfeebddc1967f4c7efc8d534dc15cfdc7259db6
Posvian <posvianski@mail.ru>
2023-07-01 22:50:37 +0200

    made hometask_12

--- common/variables.py
+++ common/variables.py
@@ -4,6 +4,7 @@ DEFAULT_IP_ADRESS = '127.0.0.1'
 MAX_CONNECTIONS = 5
 MAX_PACKAGE_LENGTH = 1024
 ENCODING = 'utf-8'
+SERVER_CONFIG = 'server.ini'
 
 ACTION = 'action'
 TIME = 'time'
@@ -16,8 +17,24 @@ PRESENCE = 'presence'
 RESPONSE = 'response'
 ERROR = 'error'
 MESSAGE = 'message'
-MESSAGE_TEXT = 'message_text'
+MESSAGE_TEXT = 'mess_text'
 EXIT = 'exit'
+GET_CONTACTS = 'get_contacts'
+LIST_INFO = 'data_list'
+REMOVE_CONTACT = 'remove'
+ADD_CONTACT = 'add'
+USERS_REQUEST = 'get_users'
 
 SERVER_DATABASE = 'sqlite:///server_base.db3'
+
+RESPONSE_202 = {RESPONSE: 202,
+                LIST_INFO:None
+                }
+
+RESPONSE_200 = {RESPONSE: 200}
+
+RESPONSE_400 = {
+            RESPONSE: 400,
+            ERROR: None
+        }
 
